440ee6eac53a177d68f8cfde771f425cbf72231c
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-06-13 18:02:13 +0530

    Created first endpoint for getting the user accounts using hono and drizzle orm.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,25 +1,24 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
+
+import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "You are not logged in.",
-    });
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
   }
 
-  return c.json({
-    message: "Hello World!",
-    userId: auth.userId,
-  });
+  return c.json({ error: "Internal error" }, 500);
 });
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
