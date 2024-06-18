341393bf12408a9d5a140d8b4b21ff3b327d57ec
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-06-18 19:57:31 +0530

    Created new endpoint for getting the transaction data for the particular id.

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -8,59 +8,103 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { db } from "@/db/drizzle";
 import { accounts, categories, transactions } from "@/db/schema";
 
-const app = new Hono().get(
-  "/",
-  clerkMiddleware(),
-  zValidator(
-    "param",
-    z.object({
-      from: z.string().optional(),
-      to: z.string().optional(),
-      acountId: z.string().optional(),
-    })
-  ),
-  async (c) => {
-    const auth = getAuth(c);
-    const { from, to, acountId } = c.req.valid("param");
-
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-
-    const defaultTo = new Date();
-    const defaultForm = subDays(defaultTo, 30);
-
-    const startDate = from
-      ? parse(from, "yyyy-MM-dd", new Date())
-      : defaultForm;
-    const toDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
-
-    const data = await db
-      .select({
-        id: transactions.id,
-        account: accounts.name,
-        accountId: transactions.accountId,
-        payee: transactions.payee,
-        notes: transactions.notes,
-        amount: transactions.amount,
-        categoryId: transactions.categoryId,
-        category: categories.name,
+const app = new Hono()
+  .get(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        from: z.string().optional(),
+        to: z.string().optional(),
+        acountId: z.string().optional(),
       })
-      .from(transactions)
-      .innerJoin(accounts, eq(transactions.accountId, accounts.id))
-      .leftJoin(categories, eq(transactions.categoryId, categories.id))
-      .where(
-        and(
-          acountId ? eq(transactions.accountId, acountId) : undefined,
-          eq(accounts.userId, auth.userId),
-          gte(transactions.date, startDate),
-          lte(transactions.date, toDate)
-        )
-      )
-      .orderBy(desc(transactions.date));
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { from, to, acountId } = c.req.valid("param");
 
-    return c.json({ data });
-  }
-);
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const defaultTo = new Date();
+      const defaultForm = subDays(defaultTo, 30);
+
+      const startDate = from
+        ? parse(from, "yyyy-MM-dd", new Date())
+        : defaultForm;
+      const toDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
+      const data = await db
+        .select({
+          id: transactions.id,
+          date: transactions.date,
+          account: accounts.name,
+          accountId: transactions.accountId,
+          payee: transactions.payee,
+          notes: transactions.notes,
+          amount: transactions.amount,
+          categoryId: transactions.categoryId,
+          category: categories.name,
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .leftJoin(categories, eq(transactions.categoryId, categories.id))
+        .where(
+          and(
+            acountId ? eq(transactions.accountId, acountId) : undefined,
+            eq(accounts.userId, auth.userId),
+            gte(transactions.date, startDate),
+            lte(transactions.date, toDate)
+          )
+        )
+        .orderBy(desc(transactions.date));
+
+      return c.json({ data });
+    }
+  )
+  .get(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Missing Id" }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .select({
+          id: transactions.id,
+          date: transactions.date,
+          accountId: transactions.accountId,
+          payee: transactions.payee,
+          notes: transactions.notes,
+          amount: transactions.amount,
+          categoryId: transactions.categoryId,
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .where(and(eq(transactions.id, id), eq(accounts.userId, auth.userId)));
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 404);
+      }
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
