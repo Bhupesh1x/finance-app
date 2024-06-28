69589a596305e4e00a3184f68b63f12b0ff91da7
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-06-28 20:55:48 +0530

    Added DateFilter component to the Filters component.

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,9 +1,11 @@
+import DateFilter from "./DateFilter";
 import { AccountFilter } from "./AccountFilter";
 
 const Filters = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-y-2 lg:gap-y-0 lg:gap-x-2 mt-4">
       <AccountFilter />
+      <DateFilter />
     </div>
   );
 };
